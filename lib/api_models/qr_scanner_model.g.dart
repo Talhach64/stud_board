61ee7d869b6a0c31107d91176b044b337519867c
Talhach64 <ch100007675@gmail.com>
2023-05-23 17:53:34 +0500

    register student section Completed

---
+++ lib/api_models/qr_scanner_model.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'qr_scanner_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+QRScannerModel _$QRScannerModelFromJson(Map<String, dynamic> json) =>
+    QRScannerModel(
+      type: json['type'] as String?,
+      message: json['message'] as String?,
+    );
+
+Map<String, dynamic> _$QRScannerModelToJson(QRScannerModel instance) =>
+    <String, dynamic>{
+      'type': instance.type,
+      'message': instance.message,
+    };
